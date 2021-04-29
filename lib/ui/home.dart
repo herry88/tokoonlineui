899e4374f24c288e86ee1dc8b12803b85da9a633
herry88 <herry081288@gmail.com>
2021-04-29 19:13:55 +0700

    NewStyles

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:zomatoui/constant/colors.dart';
+import 'package:zomatoui/constant/textstyles.dart';
+import 'package:zomatoui/ui/pertama.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +10,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = <Widget>[
+    FirstPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +53,63 @@ class _HomePageState extends State<HomePage> {
             label: 'Person',
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.blackColor,
+        unselectedItemColor: AppColors.primaryTextColorGrey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
       ),
+      body: SafeArea(
+          child: _selectedIndex == 2 || _selectedIndex == 4
+              ? Container(
+                  child: _widgetOptions.elementAt(_selectedIndex),
+                )
+              : Container(
+                  color: AppColors.whiteColor,
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                bottom: 15.0,
+                                right: 10.0,
+                              ),
+                              child: Icon(
+                                SimpleLineIcons.location_pin,
+                                size: 35.0,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                              flex: 5,
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                  left: 10.0,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Home',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyles.actionTitle,
+                                    ),
+                                  ],
+                                ),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
+                )),
     );
   }
 }
